0aac286247d52eae6446faba60a6742c62ac5c29
nas11ai <kairupannasai@gmail.com>
2026-05-16 17:15:30 +0800

    chore: init testing setup

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use App\Enums\SaleStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sale extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'created_by',
         'kode',
